2356e30df3b66f37d8e08963f4b2c042810f055c
Noktoborus <noktoborus@gmail.com>
2024-01-13 22:42:05 +0400

    update egui to 0.25 and implement undo/redo in MapPainter

--- megingjord-core/src/terminal/mappainter.rs
+++ megingjord-core/src/terminal/mappainter.rs
@@ -1,4 +1,4 @@
-use egui::{Align2, Painter, Response, RichText, Ui, Window};
+use egui::{Align2, Area, Color32, Key, Painter, Response, RichText, Ui, Window};
 use std::cell::RefCell;
 use std::rc::Rc;
 use walkers::{Plugin, Position, Projector};
@@ -25,8 +25,13 @@ impl DrawedLine {
 }
 
 struct MapPainter {
+    /// Current line.
+    /// Move points to completed at first 'drag_released' event
     current: DrawedLine,
+    /// Completed lines
     completed: Vec<DrawedLine>,
+    /// Forward history
+    forward_history: Vec<DrawedLine>,
     painting_mode_enabled: bool,
     ignore_painting: bool,
 }
@@ -36,6 +41,7 @@ impl MapPainter {
         Self {
             current: Default::default(),
             completed: Vec::new(),
+            forward_history: Vec::new(),
             painting_mode_enabled: false,
             ignore_painting: false,
         }
@@ -60,6 +66,7 @@ impl MapPainter {
         if response.drag_released_by(egui::PointerButton::Primary) {
             self.completed.push(self.current.clone());
             self.current.clear();
+            self.forward_history.clear();
         }
     }
 
@@ -88,6 +95,19 @@ impl MapPainter {
             }
         }
     }
+
+    /// Undo last drawed line
+    fn undo_line(&mut self) {
+        if let Some(last_line) = self.completed.pop() {
+            self.forward_history.push(last_line);
+        }
+    }
+
+    fn redo_line(&mut self) {
+        if let Some(next_line) = self.forward_history.pop() {
+            self.completed.push(next_line);
+        }
+    }
 }
 
 pub struct MapPainterPlugin {
@@ -114,7 +134,25 @@ impl MapPainterPlugin {
         }
     }
 
-    fn palette_ui(&mut self, ui: &mut Ui) {
+    fn palette_ui(&mut self, ui: &mut Ui, colors_and_keys: Vec<(Color32, Key)>) {
+        ui.horizontal(|ui| {
+            for (color, key) in colors_and_keys.iter() {
+                let color_button = egui::Button::new(format!("{}", key.name())).fill(*color);
+
+                if ui
+                    .add_sized(BUTTON_SIZE, color_button)
+                    .on_hover_text(format!("Shortcut: {}", key.name()))
+                    .clicked()
+                {
+                    self.active_color = *color;
+                    self.painter.borrow_mut().set_color(*color);
+                    self.show_palette = false;
+                }
+            }
+        });
+    }
+
+    fn show_ui_palette(&mut self, ui: &Ui) {
         let colors_and_keys = [
             (egui::Color32::RED, egui::Key::Num1),
             (egui::Color32::BLUE, egui::Key::Num2),
@@ -122,39 +160,94 @@ impl MapPainterPlugin {
             (egui::Color32::BROWN, egui::Key::Num4),
         ];
 
-        ui.horizontal(|ui| {
-            for (color, key) in colors_and_keys.iter() {
-                let color_button = egui::Button::new(format!("{}", key.name())).fill(*color);
+        if self.show_palette {
+            Window::new("Palette")
+                .collapsible(false)
+                .resizable(false)
+                .title_bar(false)
+                .anchor(Align2::LEFT_TOP, [54.0, 60.0])
+                .show(ui.ctx(), |ui| {
+                    self.palette_ui(ui, colors_and_keys.to_vec());
 
-            if ui
-                .add_sized(BUTTON_SIZE, color_button)
-                .on_hover_text(format!("Shortcut: {}", key.name()))
-                .clicked()
-                || ui.input(|i| i.key_pressed(*key))
-            {
-                self.active_color = *color;
-                self.painter.borrow_mut().set_color(*color);
-                self.show_palette = false;
+                    if ui.input(|i| i.key_pressed(egui::Key::Escape)) {
+                        self.show_palette = false;
+                    }
+                });
+        } else {
+            /* choose color without palette */
+            for (color, key) in colors_and_keys.iter() {
+                if ui.input(|i| i.key_pressed(*key)) {
+                    self.active_color = *color;
+                    self.painter.borrow_mut().set_color(*color);
+                    self.show_palette = false;
+                }
             }
         }
     }
 
-    fn show_palette(&mut self, window_ui: &Ui) {
-        Window::new("Palette")
-            .collapsible(false)
-            .resizable(false)
-            .title_bar(false)
-            .anchor(Align2::LEFT_TOP, [54.0, 60.0])
-            .show(window_ui.ctx(), |ui| {
-                self.palette_ui(ui);
+    fn show_ui_edit(&mut self, ui: &Ui) {
+        let (painting_mode, has_lines, has_forward_history) = {
+            let painter = self.painter.borrow();
 
-                if window_ui.input(|i| i.key_pressed(egui::Key::Escape)) {
-                    self.show_palette = false;
+            (
+                painter.painting_mode_enabled,
+                !painter.completed.is_empty(),
+                !painter.forward_history.is_empty(),
+            )
+        };
+
+        Area::new("Edits")
+            .anchor(Align2::LEFT_TOP, [16., 104.])
+            .show(ui.ctx(), |ui| {
+                if has_lines {
+                    if ui
+                        .add_sized(BUTTON_SIZE, egui::Button::new(RichText::new("S").heading()))
+                        .on_hover_text("Send figure\nShortcut: CTRL+S")
+                        .clicked()
+                        || ui.input_mut(|i| {
+                            i.consume_shortcut(&egui::KeyboardShortcut {
+                                modifiers: egui::Modifiers::CTRL,
+                                logical_key: egui::Key::S,
+                            })
+                        })
+                    {
+                        log::error!("Not implemented: Send figure");
+                    }
+                } else {
+                    ui.add_space(BUTTON_SIZE.x);
+                }
+                if painting_mode {
+                    ui.add_space(8.0);
+                    if has_forward_history {
+                        if ui
+                            .add_sized(BUTTON_SIZE, egui::Button::new(RichText::new("R").heading()))
+                            .on_hover_text("Redo\nShortcut: R")
+                            .clicked()
+                            || ui.input(|i| i.key_pressed(egui::Key::R))
+                        {
+                            self.painter.borrow_mut().redo_line();
+                        }
+                    } else {
+                        ui.add_space(BUTTON_SIZE.x);
+                    }
+                    ui.add_space(8.0);
+                    if has_lines {
+                        if ui
+                            .add_sized(BUTTON_SIZE, egui::Button::new(RichText::new("U").heading()))
+                            .on_hover_text("Undo\nShortcut: U")
+                            .clicked()
+                            || ui.input(|i| i.key_pressed(egui::Key::U))
+                        {
+                            self.painter.borrow_mut().undo_line();
+                        }
+                    } else {
+                        ui.add_space(BUTTON_SIZE.x);
+                    }
                 }
             });
     }
 
-    fn ui_edit(&mut self, ui: &mut Ui) {
+    fn ui_painting(&mut self, ui: &mut Ui) {
         if ui
             .add_sized(BUTTON_SIZE, egui::Button::new(RichText::new("🗙").heading()))
             .on_hover_text("Cancel painting\nShortcut: D or Escape")
@@ -162,6 +255,7 @@ impl MapPainterPlugin {
             || ui.input(|i| i.key_pressed(egui::Key::D))
         {
             self.painter.borrow_mut().painting_mode_enabled = false;
+            self.show_palette = false;
         }
 
         let color_button = egui::Button::new("").fill(self.active_color);
@@ -186,32 +280,34 @@ impl MapPainterPlugin {
             || ui.input(|i| i.key_pressed(egui::Key::D))
         {
             self.painter.borrow_mut().painting_mode_enabled = true;
-            self.show_palette = false;
         }
     }
 
     pub fn show_ui(&mut self, ui: &Ui) {
+        let painting_mode = self.painter.borrow().painting_mode_enabled;
+
         Window::new("Painter")
             .collapsible(false)
             .resizable(false)
             .title_bar(false)
             .anchor(Align2::LEFT_TOP, [10., 10.])
-            .show(ui.ctx(), |window_ui| {
-                if self.painter.borrow().painting_mode_enabled {
-                    if window_ui.input(|i| i.key_pressed(egui::Key::Escape)) {
+            .show(ui.ctx(), |ui| {
+                if painting_mode {
+                    if ui.input(|i| i.key_pressed(egui::Key::Escape)) {
                         if !self.show_palette {
                             self.painter.borrow_mut().painting_mode_enabled = false;
                         }
                     }
-
-                    self.ui_edit(window_ui);
-                    if self.show_palette {
-                        self.show_palette(ui);
-                    }
+                    self.ui_painting(ui);
                 } else {
-                    self.ui_short(window_ui);
+                    self.ui_short(ui);
                 }
             });
+        if painting_mode {
+            self.show_ui_palette(ui);
+        }
+
+        self.show_ui_edit(ui);
     }
 
     pub fn painting_in_progress(&self) -> bool {
